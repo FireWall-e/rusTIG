b78477822717f6b452890dcf42f249b8b35e073a
Vladyslav Slipchenko <vslipchenko@lenovo.com>
2020-03-22 13:54:27 +0200

    Main file improvements

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,6 @@ use std::process::Command;
 use std::io::{stdin, copy, prelude::Read};
 use std::fs::File;
 
-fn finish() {
-    let mut stdin = stdin();
-    println!("\nPress ENTER to exit...");
-    // Read a single byte and discard
-    stdin.read(&mut [0]).unwrap();
-}
-
 fn main() {
     // Git branch name
     let branch = "master";
@@ -37,8 +30,8 @@ fn main() {
     // Iterate through created commands
     for command_name in git_commands {
         // Full path to executable
-        git_executables_path = format!("{}/{}.exe", git_executables_path, command_name);
-        println!("Path to the new executable is: {}", git_executables_path);
+        let git_new_executable_path = format!("{}/{}.exe", git_executables_path, command_name);
+        println!("Path to the new executable is: {}", git_new_executable_path);
 
         // Build link to the githubs executable file
         let file_url = format!(
@@ -51,11 +44,21 @@ fn main() {
         // Get compiled executable content
         let mut file_content = reqwest::get(&file_url).expect("Request failed");
         // Create new empty file
-        let mut file = File::create(&git_executables_path).expect("Failed to create file");
+        let mut file = File::create(git_new_executable_path).expect("Failed to create file");
         // Put file_content inside file
         copy(&mut file_content, &mut file).expect("Failed to copy content");
     }
 
     // Terminate execution after key press
     finish();
+}
+
+fn finish() {
+    let mut stdin = stdin();
+    // Commands installation completed. Please run 'git rustig' and follow further steps to complete setup
+    println!("\nCommands installation completed!");
+    println!("Please run 'git rustig' and follow further instructions to complete setup.");
+    println!("\nPress ENTER to exit...");
+    // Read a single byte and discard
+    stdin.read(&mut [0]).unwrap();
 }
